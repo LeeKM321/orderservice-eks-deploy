65a7613f882d303134cae4de1fe5b29a71b64cef
LeeKM321 <stephen4951@gmail.com>
2024-12-24 15:24:48 +0900

    k8s폴더 삭제, 새로운 Jenkinsfile 작성(Argo CD와 연계되는) - 15

--- user-service/src/main/java/com/playdata/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/playdata/userservice/user/controller/UserController.java
@@ -179,7 +179,7 @@ public class UserController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello from user service, k8s deploy complete! - 14";
+        return "hello from user service, k8s deploy complete! - 15";
     }
 
 
